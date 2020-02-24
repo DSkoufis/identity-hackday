c6295d5880d139ebe1469f6b9934620e2b395e5f
DSkoufis <skoufisdimitris@gmail.com>
2020-02-24 13:54:30 +0000

    Create a new user endpoint

--- src/main/java/com/elsevier/id/hackathon/repository/UserDAOImpl.java
+++ src/main/java/com/elsevier/id/hackathon/repository/UserDAOImpl.java
@@ -1,12 +1,20 @@
 package com.elsevier.id.hackathon.repository;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Repository;
 
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PrimaryKey;
 import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
+import com.amazonaws.services.dynamodbv2.model.PutItemResult;
 
 @Repository
 public class UserDAOImpl implements UserDAO {
@@ -21,6 +29,13 @@ public class UserDAOImpl implements UserDAO {
 
 	@Override public Object findAttributeByUser(String userId, String locale, String attributeName) {
 		return getTable().getItem("user_id", userId).getMap(locale).get(attributeName);
+	}
+
+	@Override public void createUser(String userId) {
+		Item user = new Item();
+		user.withPrimaryKey(new PrimaryKey("user_id", userId));
+		PutItemResult res = getTable().putItem(user).getPutItemResult();
+		String a = "";
 	}
 
 	private Table getTable() {
