5db898c814bc66453d424cee8486f6b76a22dc15
DSkoufis <skoufisdimitris@gmail.com>
2020-02-24 11:47:37 +0000

    UserController first endpoint

--- src/main/java/com/elsevier/id/hackathon/controller/UserController.java
+++ src/main/java/com/elsevier/id/hackathon/controller/UserController.java
@@ -1,4 +1,21 @@
 package com.elsevier.id.hackathon.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
+
+	@GetMapping("/{user_id}/{locale}/{attribute_name}")
+	public JsonNode getAttributeByUserId(@RequestParam("user_id") String userId,
+										 @RequestParam("locale") String locale,
+										 @RequestParam("attribute_name") String attributeName) {
+
+		return null;
+	}
 }
