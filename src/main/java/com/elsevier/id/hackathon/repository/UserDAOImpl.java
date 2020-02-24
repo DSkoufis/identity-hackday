f21b27686cfb1dc5ddd47e844d051f8e562c815e
DSkoufis <skoufisdimitris@gmail.com>
2020-02-24 16:08:52 +0000

    Create/Update attribute

--- src/main/java/com/elsevier/id/hackathon/repository/UserDAOImpl.java
+++ src/main/java/com/elsevier/id/hackathon/repository/UserDAOImpl.java
@@ -1,6 +1,6 @@
 package com.elsevier.id.hackathon.repository;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,14 +12,10 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
-import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
-import com.amazonaws.services.dynamodbv2.model.AttributeAction;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.AttributeValueUpdate;
 import com.amazonaws.services.dynamodbv2.model.ExpectedAttributeValue;
 import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
 import com.amazonaws.services.dynamodbv2.model.ReturnValue;
-import com.amazonaws.services.dynamodbv2.model.UpdateItemRequest;
 
 @Repository
 public class UserDAOImpl implements UserDAO {
@@ -53,19 +49,21 @@ public class UserDAOImpl implements UserDAO {
 	@Override public void addOrUpdateAttribute(String userId, String locale, String attributeName, Object attributeValue) {
 		final boolean localeExists = getTable().getItem("user_id", userId).isPresent(locale);
 
-		if (!localeExists) {
-			//create locale
+		Map<String, Object> localeMap = new HashMap<>();
+		if (localeExists) {
+			localeMap = getTable().getItem("user_id", userId).getMap(locale);
 		}
-
-		final Map<String, Object> localeMap = getTable().getItem("user_id", userId).getMap(locale);
 
 		localeMap.put(attributeName, attributeValue);
 
 		UpdateItemSpec updateItemSpec = new UpdateItemSpec()
 				.withPrimaryKey("user_id", userId)
 				.withUpdateExpression("set " + locale + " = :lm")
-				.withValueMap(localeMap).withReturnValues(ReturnValue.UPDATED_NEW);
+				.withValueMap(Collections.singletonMap(":lm", localeMap))
+				.withReturnValues(ReturnValue.UPDATED_NEW);
+		getTable().updateItem(updateItemSpec);
 	}
+
 	private Table getTable() {
 		return dynamoDB.getTable("user");
 	}
