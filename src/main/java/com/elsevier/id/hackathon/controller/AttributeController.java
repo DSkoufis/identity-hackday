f775a10fe536e23720871040a373d8f96327d070
Tim Spencer <t.spencer@elsevier.com>
2020-02-24 11:52:01 +0000

    Added empty attribute get

--- src/main/java/com/elsevier/id/hackathon/controller/AttributeController.java
+++ src/main/java/com/elsevier/id/hackathon/controller/AttributeController.java
@@ -1,4 +1,20 @@
 package com.elsevier.id.hackathon.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+@Controller
+@RequestMapping("/attribute")
 public class AttributeController {
+	@GetMapping("/{locale}/{attribute_name}")
+	public JsonNode getAttributeValues(
+			@RequestParam("locale") String locale,
+			@RequestParam("attribute_name") String attributeName) {
+
+		return null;
+	}
 }
